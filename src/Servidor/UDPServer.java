aaf54f2cadfa30e40347b4f5cecf3225d861b8db
silmarakelly <49078429+silmarakelly@users.noreply.github.com>
2019-11-28 12:02:12 -0300

    Update UDPServer.java

--- src/Servidor/UDPServer.java
+++ src/Servidor/UDPServer.java
@@ -3,7 +3,6 @@ package Servidor;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
 import java.util.Scanner;
 
 /*public class UDPServer {
@@ -79,7 +78,7 @@ public class UDPServer {
 		DatagramPacket recebimento = null;
 		boolean fez = false;
 		String info = "";
-		
+
 		try {
 			aSocket = new DatagramSocket(33000);
 
@@ -93,30 +92,33 @@ public class UDPServer {
 					if (info.equals("b")) {
 						String msgrecebe = op[1];
 						byte[] msfinal = msgrecebe.getBytes();
-						DatagramPacket msgBroad = new DatagramPacket(msfinal, msfinal.length,recebimento.getAddress(),recebimento.getPort());
+						DatagramPacket msgBroad = new DatagramPacket(msfinal, msfinal.length, recebimento.getAddress(),
+								recebimento.getPort());
 						fez = true;
 						new Thread(new ThreadBroadcast(aSocket, msgBroad)).start();
-					
-					} 
-					
-				}
+
+					}
+
+				} else {
 					recebimento = new DatagramPacket(bufferchat, bufferchat.length);
 					aSocket.receive(recebimento);
 					String receive = new String(recebimento.getData());
 					op = receive.split("/");
 					info = op[0];
-					if(info.equals("c")) {
+					if (info.equals("c")) {
 						System.out.println("Mensagem de: " + recebimento.getAddress());
 						String msgrecebe = op[1];
 						System.out.println(msgrecebe);
 						byte[] msfinal = msgrecebe.getBytes();
-						DatagramPacket msgchat = new DatagramPacket(msfinal, msfinal.length, recebimento.getAddress(), recebimento.getPort());
+						DatagramPacket msgchat = new DatagramPacket(msfinal, msfinal.length, recebimento.getAddress(),
+								recebimento.getPort());
 						System.out.println("-------------------------------------------");
 
 						new Thread(new ThreadChat(aSocket, msgchat)).start();
 
-				}
+					}
 
+				}
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -131,33 +133,41 @@ class ThreadBroadcast implements Runnable {
 
 	DatagramSocket socket = null;
 	DatagramPacket packet = null;
-	String[] conectados = null;
+	String[] conectados = new String[1024];
 	Scanner entrada = new Scanner(System.in);
 	byte[] buffer2 = new byte[1024];
 	int cont = 0;
+
 	public ThreadBroadcast(DatagramSocket socket, DatagramPacket packet) {
 		this.socket = socket;
 		this.packet = packet;
 	}
 
 	public void run() {
-		
+
 		String dados = new String(packet.getData());
-		String [] info = dados.split(":");
+		String[] info = dados.split(":");
 		String mac = info[0];
 		String nome = info[1];
-		System.out.println("conectados: "+nome);
-		//conectados[0] = conectados[cont]+dados+"/n";
-		//cont = cont++;
-		//for (int i=0; i<cont; i++) {
-			//System.out.println(conectados[i]);
-			
-		//}
-		System.out.println("Deseja conectar-se com " + nome + "?(sim/não)");
-		String desejo = entrada.nextLine();
-		
-		
-		
+		if (conectados[cont] == null) {
+			conectados[cont] = nome + "/n";
+			cont = cont++;
+
+		} else {
+			conectados[cont] = conectados[cont] + nome + "/n";
+			 cont = cont++;
+
+
+		}
+		// for (int i=0; i<cont; i++) {
+		// System.out.println(conectados[i]);
+
+		// }
+		System.out.println("conectados: " + conectados[cont]);
+
+		// System.out.println("Deseja conectar-se com " + conectados[cont] );
+		String desejo = "sim";
+
 		try {
 			if (desejo.equalsIgnoreCase("sim")) {
 				System.out.println("Você: ");
@@ -173,26 +183,23 @@ class ThreadBroadcast implements Runnable {
 				System.out.println("Mensagem enviada");
 				System.out.println("-------------------------------------------");
 			}
-		}catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 	}
 }
 
 class ThreadChat implements Runnable {
-	
+
 	DatagramPacket packet = null;
 	DatagramSocket socket = null;
 	Scanner entrada = new Scanner(System.in);
 
-
 	public ThreadChat(DatagramSocket socket, DatagramPacket packet) {
 		this.socket = socket;
 		this.packet = packet;
-	
 
 	}
 
@@ -210,7 +217,7 @@ class ThreadChat implements Runnable {
 
 			System.out.println("Mensagem enviada");
 			System.out.println("-------------------------------------------");
-		}catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
